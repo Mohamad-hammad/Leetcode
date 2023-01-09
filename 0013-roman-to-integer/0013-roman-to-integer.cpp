472a95e716642f727d34bde6760580d4b9b25ffa
Hammad <66684303+Mohamad-hammad@users.noreply.github.com>
2023-01-09 23:12:29 +0500

    Time: 20 ms (46.77%), Space: 8 MB (42.13%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> umap;
+        umap['I'] = 1;
+        umap['V'] = 5;
+        umap['X'] = 10;
+        umap['L'] = 50;
+        umap['C'] = 100;
+        umap['D'] = 500;
+        umap['M'] = 1000;
+        int result = umap.at(s[s.length()-1]);
+        for(int i = s.length()-2;i>=0;i--)
+        {
+            if(umap.at(s[i]) < umap.at(s[i+1]))
+               result -= umap.at(s[i]);
+            
+            else
+                result += umap.at(s[i]);
+        }
+        return result;
+    }
+};
