922d799e9115f2eb3cceba0a1600d291b5b7f156
Hammad <66684303+Mohamad-hammad@users.noreply.github.com>
2023-01-29 00:08:59 +0500

    Time: 103 ms (66.15%), Space: 59.1 MB (5.66%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -20,26 +20,3 @@ public:
         return maxarea;
     }
 };
-/*
- int maxArea(vector<int>& height) {
-        int start = 0;
-        int end = height.size()-1;
-        int area;
-        int maxarea=0;
-        while(start<end)
-        {
-            area = min(height[start],height[end])*(end-start);
-            if(area>maxarea)
-                maxarea=area;
-            if((min(height[start+1],height[end])*(end-start-1))>area)
-                start++;
-            else if ((min(height[start],height[end-1])*(end-1-start))>area)
-                end--;
-            else
-                return area;
-            
-          
-        }
-        return area;
-    }
-                */
